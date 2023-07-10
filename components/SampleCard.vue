9c3645cc2187a431032804496df38cf4bce5bf98
mwbernard <mwbernard9@gmail.com>
2023-07-10 13:26:04 -0600

    Add flip functionality to sample cards

--- components/SampleCard.vue
+++ components/SampleCard.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="sample-wrapper">
-    <div
-      class="front-side"
-      :style="{ backgroundImage: `url(${backgroundImage})` }"
-    >
-      <div class="title"></div>
+  <div class="card-wrapper">
+    <div class="card-inner" :class="{ flipped }" @click="flipCard">
       <div
-        class="hero-image"
-        :style="{ backgroundImage: `url(${heroImage})` }"
-      ></div>
-    </div>
-    <div class="back-side">
-      <div class="description"></div>
-      <div class="parents"></div>
-      <div class="authors"></div>
+        class="front-side"
+        :style="{ backgroundImage: `url(${backgroundImage})` }"
+      >
+        <div class="title"></div>
+        <div
+          class="hero-image"
+          :style="{ backgroundImage: `url(${heroImage})` }"
+        ></div>
+      </div>
+      <div class="back-side">
+        <div class="description"></div>
+        <div class="parents"></div>
+        <div class="authors"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,20 +25,24 @@ export default {
   name: 'SampleCard',
   props: {
     id: {
-      type: Number,
-      required: true,
+      type: String,
+      required: false,
+      default: '',
     },
     type: {
       type: String,
-      required: true,
+      required: false,
+      default: '',
     },
     title: {
       type: String,
-      required: true,
+      required: false,
+      default: '',
     },
     heroImage: {
       type: String,
-      required: true,
+      required: false,
+      default: '',
     },
     backgroundImage: {
       type: String,
@@ -45,19 +51,69 @@ export default {
     },
     description: {
       type: String,
-      required: true,
+      required: false,
+      default: '',
     },
     author: {
       type: Array,
-      required: true,
+      required: false,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      flipped: false,
+    }
+  },
+  methods: {
+    flipCard() {
+      this.flipped = !this.flipped
     },
   },
 }
 </script>
 
 <style scoped>
-.sample-wrapper {
+.card-wrapper {
+  background-color: transparent;
+  width: var(--card-width);
+  height: var(--card-height);
+  /* perspective: 1000px; */ /* 3d effect */
   display: flex;
   flex-direction: column;
+}
+
+.card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  transition: transform 1s;
+  transform-style: preserve-3d;
+}
+
+.card-inner.flipped {
+  transform: rotateY(180deg);
+}
+
+.front-side,
+.back-side {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+.front-side {
+  border: 1px var(--text) solid;
+  background-color: white;
+  color: black;
+}
+
+.back-side {
+  background-color: black;
+  color: white;
+  transform: rotateY(180deg);
 }
 </style>
