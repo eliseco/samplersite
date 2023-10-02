5968dfa475ba5b310e161e697851b1049261e848
mwbernard <mwbernard9@gmail.com>
2023-10-02 16:57:43 -0700

    Start updating set page

--- pages/sets/_setId.vue
+++ pages/sets/_setId.vue
@@ -7,35 +7,54 @@ this page will then access the set from our store using the passed in setID, and
  -->
 
 <template>
-  <main class="set-wrapper">
-    <div class="header">
-      {{ project.title }}
-    </div>
-    <div class="description">
-      {{ project.description }}
-    </div>
-    <div class="hero-images">
-      <div
-        v-for="image in project.heroImages"
-        :key="image"
-        class="hero-image"
-        :style="{ backgroundImage: `url(/images/projects/${image})` }"
-      />
+  <main
+    class="set-wrapper"
+    :style="{
+      backgroundImage: `url(/images/background/${project.pageBackground})`,
+    }"
+  >
+    <div class="top-section">
+      <div class="information">
+        <div class="header">
+          {{ project.title }}
+        </div>
+        <div class="description">
+          {{ project.description }}
+        </div>
+      </div>
+      <ImageCarousel :images="project.heroImages" />
     </div>
 
-    <div class="children">
-      <SampleCard
-        v-for="(card, i) in children"
-        :key="i"
-        :sid="card.id"
-        :type="card.type"
-        :title="card.title"
-        :hero-image="card.heroImages[0]"
-        :tags="card.tags"
-        :description="card.description"
-        :authors="card.author"
-        :background-image="card.type === 'project' ? card.background : null"
-      />
+    <div class="sample-carousel">
+      <h2>SAMPLES</h2>
+      <div class="children">
+        <SampleCard
+          v-for="(card, i) in children"
+          :key="i"
+          :sid="card.id"
+          :type="card.type"
+          :title="card.title"
+          :hero-image="card.heroImages[0]"
+          :tags="card.tags"
+          :description="card.description"
+          :authors="card.author"
+          :background-image="card.type === 'project' ? card.background : null"
+        />
+      </div>
+    </div>
+
+    <div class="questions">
+      <h3 v-for="q in project.researchQuestions" :key="q">
+        {{ q }}
+      </h3>
+    </div>
+
+    <div class="process-images">
+      <h2>PROCESS</h2>
+    </div>
+
+    <div class="team">
+      <h2>TEAM MEMBERS</h2>
     </div>
   </main>
 </template>
@@ -63,41 +82,63 @@ export default {
   padding: var(--padding);
   display: flex;
   flex-direction: column;
-  gap: var(--padding);
   overflow-y: auto;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: -1px -1px;
+  background-attachment: local;
+}
+
+.top-section {
+  display: flex;
+  flex-direction: row;
+}
+
+.information {
+  flex-shrink: 0;
+  width: 400px;
+  padding: var(--padding);
 }
 
 .header {
   font-family: var(--rajdhani);
   font-size: 32px;
   font-weight: 600;
+  color: var(--active-green);
 }
 
-.hero-images {
-  flex-shrink: 0;
-  display: flex;
-  flex-direction: row;
-  overflow-x: auto;
-  gap: var(--padding);
+h2 {
+  color: white;
+  font-family: var(--rajdhani);
+  font-weight: 700;
+  font-size: 40px;
 }
 
-.hero-image {
-  flex-shrink: 0;
-  height: 400px;
-  width: 600px;
-  background-size: cover;
-  background-repeat: no-repeat;
+h3 {
+  color: white;
+  font-family: var(--rajdhani);
+  font-weight: 700;
+  font-size: 28px;
 }
 
 .description {
   font-family: var(--roboto);
+  color: white;
 }
 
+.sample-carousel {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
 .children {
+  align-self: center;
+  width: 90%;
   flex-shrink: 0;
   display: flex;
   flex-direction: row;
   gap: var(--padding);
   overflow-x: auto;
+  height: calc(1.1 * var(--card-height));
 }
 </style>
